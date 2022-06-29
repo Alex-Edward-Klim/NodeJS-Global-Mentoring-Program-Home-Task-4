d3df963489e44b38824fec593c15687d42bbd155
Alexey Klimovskoy <Alexey_Klimovskoy@epam.com>
2022-06-29 21:13:59 +0300

    feat: implement addUsersToGroup

--- src/routes/addUsersToGroup.ts
+++ src/routes/addUsersToGroup.ts
@@ -2,21 +2,21 @@ import express, { Request, Response } from 'express';
 
 import { Group, User } from '../models/index';
 
-const addUsersToGroup = async (groupId: any, userId: any) => {
+const addUsersToGroup = async (groupId: any, userIds: any) => {
   const group: any = await Group.findOne({
     where: {
       id: groupId,
     },
   });
 
-  const user: any = await User.findOne({
+  const users: any = await User.findAll({
     where: {
-      id: userId,
+      id: userIds,
     },
   });
 
-  if (group && user) {
-    return group.addUser(user);
+  if (group && users.length > 0) {
+    return group.addUsers(users);
   }
   return null;
 };
@@ -24,14 +24,14 @@ const addUsersToGroup = async (groupId: any, userId: any) => {
 const router = express.Router();
 
 router.post('/', async (req: Request, res: Response) => {
-  const { groupId, userId } = req.body;
+  const { groupId, userIds } = req.body;
 
-  const UserAddedToGroup = await addUsersToGroup(groupId, userId);
+  const UsersAddedToGroup = await addUsersToGroup(groupId, userIds);
 
-  if (!UserAddedToGroup) {
-    res.status(500).send('Err');
+  if (!UsersAddedToGroup) {
+    res.status(500).send('Error: wrong request data');
   } else {
-    res.status(200).send('User Added To Group');
+    res.status(200).send('Users Added To Group');
   }
 });
 
