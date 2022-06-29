23347885508999c22e1bd6772c6a10b22cc1eee6
Alexey Klimovskoy <Alexey_Klimovskoy@epam.com>
2022-06-29 22:49:29 +0300

    feat: implement multiple layer architecture for addUsersToGroup and removeUsersFromGroup

--- src/routes/addUsersToGroup.ts
+++ src/routes/addUsersToGroup.ts
@@ -1,49 +1,9 @@
-import express, { Request, Response } from 'express';
+import express from 'express';
 
-import dataBase from '../data-access/dataBase';
-
-import { Group, User } from '../models/index';
-
-const addUsersToGroup = async (groupId: any, userIds: any) => {
-  const t = await dataBase.transaction();
-
-  try {
-    const group: any = await Group.findOne({
-      where: {
-        id: groupId,
-      },
-    });
-
-    const users: any = await User.findAll({
-      where: {
-        id: userIds,
-      },
-    });
-
-    if (group && users.length > 0) {
-      const UsersAddedToGroup = await group.addUsers(users);
-      await t.commit();
-      return UsersAddedToGroup;
-    }
-    throw new Error('Incorrect request data');
-  } catch (err) {
-    await t.rollback();
-    throw err;
-  }
-};
+import addUsersToGroupController from '../controllers/addUsersToGroupController';
 
 const router = express.Router();
 
-router.post('/', async (req: Request, res: Response) => {
-  const { groupId, userIds } = req.body;
-
-  const UsersAddedToGroup = await addUsersToGroup(groupId, userIds);
-
-  if (!UsersAddedToGroup) {
-    throw new Error('Incorrect request data');
-  } else {
-    res.status(200).send('Users Added To Group');
-  }
-});
+router.post('/', addUsersToGroupController.addUsersToGroup);
 
 export default router;
--- src/routes/removeUsersFromGroup.ts
+++ src/routes/removeUsersFromGroup.ts
@@ -1,49 +1,9 @@
-import express, { Request, Response } from 'express';
+import express from 'express';
 
-import dataBase from '../data-access/dataBase';
-
-import { Group, User } from '../models/index';
-
-const removeUsersFromGroup = async (groupId: any, userIds: any) => {
-  const t = await dataBase.transaction();
-
-  try {
-    const group: any = await Group.findOne({
-      where: {
-        id: groupId,
-      },
-    });
-
-    const users: any = await User.findAll({
-      where: {
-        id: userIds,
-      },
-    });
-
-    if (group && users.length > 0) {
-      const UsersRemovedToGroup = await group.removeUsers(users);
-      await t.commit();
-      return UsersRemovedToGroup;
-    }
-    throw new Error('Incorrect request data');
-  } catch (err) {
-    await t.rollback();
-    throw err;
-  }
-};
+import removeUsersFromGroupController from '../controllers/removeUsersFromGroupController';
 
 const router = express.Router();
 
-router.post('/', async (req: Request, res: Response) => {
-  const { groupId, userIds } = req.body;
-
-  const UsersRemovedToGroup = await removeUsersFromGroup(groupId, userIds);
-
-  if (!UsersRemovedToGroup) {
-    throw new Error('Incorrect request data');
-  } else {
-    res.status(200).send('Users Removed From Group');
-  }
-});
+router.post('/', removeUsersFromGroupController.removeUsersFromGroup);
 
 export default router;
